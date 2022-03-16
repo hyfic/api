54472902d9bbf31d01279bdb083d54fc3f4f5c5b
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-16 21:46:04 +0530

    user mutation

---
+++ src/graphql/mutations/user.mutation.ts
@@ -0,0 +1,63 @@
+import { GraphQLBoolean, GraphQLString } from 'graphql';
+import { LoginArgs, RegisterArgs, UpdateArgs } from '../../utils/types';
+import { UserType } from '../typeDefs/user.typeDef';
+import {
+  loginController,
+  registerController,
+  updateController,
+} from '../../controllers/user.controller';
+import {
+  validateLoginArgs,
+  validateRegisterArgs,
+} from '../validators/user.validator';
+import { ExpressContext } from 'apollo-server-express';
+import { getUserFromContext } from '../../utils/jwt';
+
+export const LOGIN = {
+  type: UserType,
+  args: {
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(parent: any, requestArgs: any) {
+    const args: LoginArgs = validateLoginArgs(requestArgs);
+    return loginController(args);
+  },
+};
+
+export const REGISTER = {
+  type: UserType,
+  args: {
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    profile: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(parent: any, requestArgs: any) {
+    const args: RegisterArgs = validateRegisterArgs(requestArgs);
+    return registerController(args);
+  },
+};
+
+export const UPDATE = {
+  type: UserType,
+  args: {
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    profile: { type: GraphQLString },
+    password: { type: GraphQLString },
+    isNewPassword: { type: GraphQLBoolean },
+  },
+  async resolve(parent: any, requestArgs: any, context: ExpressContext) {
+    const loggedInUser: any = getUserFromContext(context);
+    const user: UpdateArgs = {
+      ...loggedInUser,
+      ...requestArgs,
+      password: requestArgs.isNewPassword
+        ? requestArgs.password
+        : loggedInUser.password,
+      ogEmail: loggedInUser.email,
+    };
+    return updateController(user);
+  },
+};
