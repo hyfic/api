65eec22995d0c7b80336723065a5c38c0d619f20
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-16 21:39:03 +0530

    user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,50 @@
+import { UserInputError } from 'apollo-server-core';
+import { generateToken } from '../utils/jwt';
+import { LoginArgs, RegisterArgs, UpdateArgs } from '../utils/types';
+import {
+  findUser,
+  loginUser,
+  registerUser,
+  updateUser,
+} from '../models/user.model';
+
+export const registerController = async (args: RegisterArgs) => {
+  const user = await registerUser(args).catch((err) => {
+    throw new UserInputError(err);
+  });
+
+  return {
+    ...user,
+    token: generateToken(user),
+  };
+};
+
+export const loginController = async (args: LoginArgs) => {
+  const user = await loginUser(args).catch((err) => {
+    throw new UserInputError(err);
+  });
+
+  return {
+    ...user,
+    token: generateToken(user),
+  };
+};
+
+export const updateController = async (args: UpdateArgs) => {
+  const user = await updateUser(args).catch((err) => {
+    throw new UserInputError(err);
+  });
+
+  return {
+    ...user,
+    token: generateToken(user),
+  };
+};
+
+export const getUserController = async (email: string) => {
+  const user = await findUser(email).catch((err) => {
+    throw new UserInputError(err);
+  });
+
+  return user;
+};
