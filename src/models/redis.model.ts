a44381c63f675a317fb2676a712e63bbc17efca9
fadhilsaheer <fadhilsaheer@gmail.com>
2022-03-18 07:06:12 +0530

    FIX: redis changelog

--- src/models/redis.model.ts
+++ src/models/redis.model.ts
@@ -3,41 +3,41 @@ import { client } from '../redis';
 // { "socketID": { roomId: "some room Id", userId: "user id" } }
 export const setData = (socketId: string, roomId: string, userId: number) => {
   return new Promise((resolve, reject) => {
-    client.set(
-      socketId,
-      JSON.stringify({ roomId, userId }),
-      (_: any, reply: unknown) => {
-        if (reply) resolve(reply);
-        else reject('Failed to set data');
-      }
-    );
+    client
+      .set(socketId, JSON.stringify({ roomId, userId }))
+      .then(resolve)
+      .catch(() => reject('Failed to set data'));
   });
 };
 
 export const getData = (socketId: string) => {
   return new Promise((resolve, reject) => {
-    client.get(socketId, (err: any, reply: string) => {
-      if (err || !reply) {
+    client
+      .get(socketId)
+      .then((reply) => {
+        if (!reply) throw new Error('Failed to get data');
+
+        const { roomId, userId } = JSON.parse(reply);
+
+        resolve({
+          socketId,
+          roomId,
+          userId,
+        });
+      })
+      .catch(() => {
         reject(`Failed to get data for ${socketId}`);
-        return;
-      }
-
-      const { roomId, userId } = JSON.parse(reply);
-
-      resolve({
-        socketId,
-        roomId,
-        userId,
       });
-    });
   });
 };
 
 export const deleteData = (socketId: string) => {
   return new Promise((resolve, reject) => {
-    client.del(socketId, (_: any, reply: unknown) => {
-      if (reply) resolve(reply);
-      else reject(`Failed to delete with key ${socketId}`);
-    });
+    client
+      .del(socketId)
+      .then(resolve)
+      .catch(() => {
+        reject(`Failed to delete with key ${socketId}`);
+      });
   });
 };
